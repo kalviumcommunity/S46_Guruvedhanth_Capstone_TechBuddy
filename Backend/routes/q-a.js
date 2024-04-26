33cd50de2010ea28b7ff7cfcc7f8711e9c68f2bf
guruvedhanth-s <guruvedhanths@gmail.com>
2024-04-26 14:38:28 +0530

    Update and delete method for answers

--- Backend/routes/q-a.js
+++ Backend/routes/q-a.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const {addquestion,addanswer,getQuestion, deleteQuestion, updateQuestion} = require('../controllers/q-aController')
+const {addquestion,addanswer,getQuestion, deleteQuestion, updateQuestion,deleteAnswer,updateAnswer} = require('../controllers/q-aController')
 
 router.post("/question",addquestion)
 
@@ -13,5 +13,9 @@ router.get("/queries",getQuestion)
 router.delete("/delquestion/:id",deleteQuestion)
 
 router.patch("/updatequestion/:id",updateQuestion)
+
+router.delete("/delanswer/:id",deleteAnswer)
+
+router.patch("/updateanswer/:id",updateAnswer)
 
 module.exports = router
